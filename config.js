6743c715d65bc7dd9d3997082450db1f76162fd1
Logan Wholey <lwholeyumw@gmail.com>
2020-02-24 15:32:45 -0800

    first commit

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: "Logan Wholey's Resume", // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/wholey-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Logan',
+  lastName: 'Wholey',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/theanubhav/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
+  email: 'logan.wholey@gmail.com',
+  phone: '000-00000',
+  address: 'Seattle, WA',
+};
