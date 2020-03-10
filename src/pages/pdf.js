eb7eccea1908d778d6d0b94565618964cb779a90
Logan Wholey <lwholeyumw@gmail.com>
2020-03-10 10:33:05 -0700

    fixing pdf loading issues

---
+++ src/pages/pdf.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
+import Layout from '../components/Layout';
+import MyResume from '../components/pdf/MyResume';
+
+class PDF extends Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: false });
+  }
+
+  renderSpinner = () => <i class="fas fa-spinner spinner rotate"></i>;
+
+  renderPDFViewer = () => (
+    <div className="pdf-view">
+      <PDFViewer style={{ height: '100vh', width: '100vw' }}>
+        <MyResume />
+      </PDFViewer>
+    </div>
+  );
+
+  render() {
+    return (
+      <Layout isPDF>
+        <div className="pdf-page">
+          {this.state.loading ? this.renderSpinner() : this.renderPDFViewer()}
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default PDF;
